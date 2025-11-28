a268bae3bc63a3b019e11ab4a922e216a679832e
datdenkikniet <jcdra1@gmail.com>
2025-11-28 17:07:40 +0100

    This is probably more correct

--- src/error.rs
+++ src/error.rs
@@ -2,11 +2,7 @@ pub type Result<T> = std::result::Result<T, RadosError>;
 
 #[must_use]
 pub fn maybe_err(value: i32) -> Result<()> {
-    if value == 0 {
-        Ok(())
-    } else {
-        Err(value.into())
-    }
+    (value > 0).then_some(()).ok_or(value.into())
 }
 
 #[derive(Debug, Clone, PartialEq)]
