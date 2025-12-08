2c7487665f32d1ff57b4c1bfd1bd32aa1ff18518
datdenkikniet <jcdra1@gmail.com>
2025-12-08 18:52:10 +0100

    Fmt ByteCount

--- src/bytecount.rs
+++ src/bytecount.rs
@@ -26,3 +26,26 @@ impl std::ops::Add for ByteCount {
         ByteCount(self.0 + rhs.0)
     }
 }
+
+impl core::fmt::Display for ByteCount {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let bytes = self.0;
+
+        const KIB_BOUND: u64 = 1024;
+        const MIB_BOUND: u64 = 512 * KIB_BOUND;
+        const GIB_BOUND: u64 = 1024 * MIB_BOUND;
+        const TIB_BOUND: u64 = 1024 * GIB_BOUND;
+
+        let (pow, sf) = match bytes {
+            0..KIB_BOUND => (0, "b"),
+            KIB_BOUND..MIB_BOUND => (1, "KiB"),
+            KIB_BOUND..GIB_BOUND => (2, "MiB"),
+            GIB_BOUND..TIB_BOUND => (3, "GiB"),
+            _ => (4, "TiB"),
+        };
+
+        let div = 1024u64.pow(pow);
+
+        write!(f, "{:.01} {}", bytes as f64 / div as f64, sf)
+    }
+}
