15e8a653a1540cc9719dc99735b9c3e5d8fd977b
datdenkikniet <jcdra1@gmail.com>
2025-12-15 21:44:08 +0100

    Well, CRC32-C is FreeBSD's crc32.c, not CRC-32/CASTAGNOLI

--- ceph-protocol/examples/handshake.rs
+++ ceph-protocol/examples/handshake.rs
@@ -25,15 +25,17 @@ fn main() {
     println!("RX banner: {rx_banner:?}");
 
     let mut hello_buffer = [0u8; 128];
-    let hello_frame = Frame::new(&[&[]]).unwrap();
+    let hello_frame = Frame::new(Tag::Hello, &[&[]]).unwrap();
 
-    let len = hello_frame.write(Tag::Hello, &mut hello_buffer).unwrap();
+    let len = hello_frame.write(&mut hello_buffer).unwrap();
 
     stream.write_all(&hello_buffer[..len]).unwrap();
 
     std::thread::sleep(Duration::from_millis(50));
 
     let hello_response = stream.read(&mut hello_buffer).unwrap();
+
+    println!("Data: {:02X?}", &hello_buffer[..hello_response]);
 
     let hello_response = Frame::parse(&hello_buffer[..hello_response]).unwrap();
 
