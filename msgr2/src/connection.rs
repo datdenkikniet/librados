18d9fd87e8ef8872a3b83e0c9b210bf8aaefd7bd
datdenkikniet <jcdra1@gmail.com>
2025-12-27 15:34:05 +0100

    State-machine

--- msgr2/src/connection.rs
+++ msgr2/src/connection.rs
@@ -8,77 +8,76 @@ use crate::{
     },
 };
 
-enum State {
-    Inactive,
-    Active,
-}
-
-impl State {
-    fn is_active(&self) -> bool {
-        matches!(self, Self::Active)
-    }
-
-    fn is_inactive(&self) -> bool {
-        matches!(self, Self::Inactive)
-    }
-}
-
-pub struct Connection {
+#[derive(Debug, Clone)]
+pub struct Config {
     support_rev21: bool,
-    state: State,
+}
+
+impl Config {
+    pub fn new(support_rev21: bool) -> Self {
+        Self { support_rev21 }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Inactive {
+    config: Config,
+}
+
+#[derive(Debug)]
+pub struct Active {
     revision: Msgr2Revision,
+}
+
+#[derive(Clone, Debug)]
+pub struct Connection<T> {
+    state: T,
     buffer: Vec<u8>,
 }
 
-impl Connection {
-    pub fn new() -> Self {
+impl Connection<Inactive> {
+    pub fn new(config: Config) -> Self {
         Self {
-            support_rev21: true,
-            state: State::Inactive,
-            revision: Msgr2Revision::V2_0,
+            state: Inactive { config },
             buffer: Vec::new(),
         }
-    }
-
-    pub fn support_rev21(&mut self, support_rev21: bool) {
-        assert!(self.state.is_inactive());
-        self.support_rev21 = support_rev21;
     }
 
     pub fn banner(&self) -> Banner {
         let mut features = MsgrFeatures::empty();
 
-        if self.support_rev21 {
+        if self.state.config.support_rev21 {
             features.set_revision_21(true);
         }
 
         Banner::new(features, MsgrFeatures::empty())
     }
 
+    pub fn recv_banner(self, banner: &Banner) -> Result<Connection<Active>, String> {
+        if banner.required().compression() {
+            return Err("Peer requires compression, which we do not support.".into());
+        }
+
+        let revision = if self.state.config.support_rev21 && banner.supported().revision_21() {
+            Msgr2Revision::V2_1
+        } else {
+            Msgr2Revision::V2_0
+        };
+
+        Ok(Connection {
+            state: Active { revision },
+            buffer: Vec::new(),
+        })
+    }
+}
+
+impl Connection<Active> {
     pub fn preamble_len(&self) -> usize {
         // Fixed-size preamble for now
         crate::frame::Preamble::SERIALIZED_SIZE
     }
 
-    pub fn recv_banner(&mut self, banner: &Banner) -> Result<(), String> {
-        assert!(self.state.is_inactive());
-
-        if banner.required().compression() {
-            return Err("Peer requires compression, which we do not support.".into());
-        }
-
-        if self.support_rev21 && banner.supported().revision_21() {
-            self.revision = Msgr2Revision::V2_1;
-        }
-
-        self.state = State::Active;
-
-        Ok(())
-    }
-
     pub fn recv_preamble(&mut self, preamble_data: &[u8]) -> Result<Preamble, String> {
-        assert!(self.state.is_active());
-
         if preamble_data.len() != self.preamble_len() {
             return Err(format!(
                 "Expected {} bytes of preamble data, got {}",
@@ -87,13 +86,12 @@ impl Connection {
             ));
         }
 
-        let preamble = Preamble::parse(preamble_data, self.revision)?;
+        let preamble = Preamble::parse(preamble_data, self.state.revision)?;
 
         Ok(preamble)
     }
 
     pub fn recv(&mut self, frame: Frame) -> Result<Message, String> {
-        assert!(self.state.is_active());
         assert!(
             frame.segments().len() == 1,
             "Multi-segment frames not supported yet."
@@ -109,12 +107,10 @@ impl Connection {
     }
 
     pub fn send_msg<'a>(&'a mut self, message: &Message) -> Frame<'a> {
-        assert!(self.state.is_active());
-
         self.buffer.clear();
         message.write_to(&mut self.buffer);
 
-        Frame::new(message.tag(), &[&self.buffer], self.revision).unwrap()
+        Frame::new(message.tag(), &[&self.buffer], self.state.revision).unwrap()
     }
 }
 
