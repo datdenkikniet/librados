2b0551bd2b6052ea1d1b32445b34b1dd88098877
datdenkikniet <jcdra1@gmail.com>
2026-01-10 16:33:37 +0100

    Remove not-as-useful librados wrapper

--- src/iter_objects/mod.rs
+++
@@ -1,7 +0,0 @@
-mod basic;
-mod cursors;
-mod object;
-
-pub use basic::ObjectsIterator;
-pub use cursors::Cursor;
-pub use object::{ListObject, OwnedObject, RawObject, RefObject};
--- src/read/mod.rs
+++
@@ -1,7 +0,0 @@
-mod omapvals;
-mod read;
-mod read_op;
-mod stat;
-
-pub use omapvals::OmapKeyValues;
-pub use stat::Stat;
