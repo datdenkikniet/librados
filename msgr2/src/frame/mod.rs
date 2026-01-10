a34d138eacda70359ebe680b0e622fc5170b57f4
datdenkikniet <jcdra1@gmail.com>
2026-01-10 16:25:43 +0100

    Move frame stuff into frame mod

--- msgr2/src/frame/mod.rs
+++ msgr2/src/frame/mod.rs
@@ -1,20 +1,20 @@
 mod encryption;
 mod epilogue;
-mod frame;
 mod preamble;
 mod wire;
 
-use std::num::NonZeroUsize;
+use std::num::{NonZeroU8, NonZeroUsize};
 
 pub(crate) use encryption::FrameEncryption;
 pub(crate) use epilogue::Epilogue;
-pub use frame::Frame;
 pub(crate) use preamble::Preamble;
 
 pub use preamble::Tag;
 
 pub use encryption::{DecryptError, EncryptError};
 pub use wire::{Completed, ReadPreamble, RxError, RxFrame, TxError, TxFrame, Unstarted};
+
+use crate::{DecodeError, frame::preamble::SegmentDetail};
 
 pub const REV1_SECURE_INLINE_SIZE: usize = 48;
 pub const REV1_SECURE_PAD_SIZE: NonZeroUsize = NonZeroUsize::new(16).unwrap();
@@ -46,22 +46,253 @@ impl FrameFormat {
     }
 }
 
-#[test]
-fn valid_frame() {
-    use crate::frame::preamble::Preamble;
+const ALGO: crc::Algorithm<u32> = crc::Algorithm {
+    width: 32,
+    poly: 0x1EDC6F41,
+    init: u32::MAX,
+    refin: true,
+    refout: true,
+    xorout: 0,
+    check: 0,
+    residue: 0,
+};
 
-    let frame_data = &[
-        01, 01, 36, 00, 00, 00, 08, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-        00, 00, 00, 00, 00, 63, 189, 107, 06, 01, 01, 01, 01, 28, 00, 00, 00, 02, 00, 00, 00, 00,
-        00, 00, 00, 16, 00, 00, 00, 02, 00, 221, 90, 10, 00, 01, 05, 00, 00, 00, 00, 00, 00, 00,
-        00, 00, 105, 92, 102, 236, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
-    ];
+const EMPTY: &[u8] = &[];
+const CRC: crc::Crc<u32> = crc::Crc::<u32>::new(&ALGO);
 
-    let rev = FrameFormat::Rev0Crc;
-    let (preamble, frame_data) = frame_data
-        .split_first_chunk::<{ Preamble::SERIALIZED_SIZE }>()
-        .unwrap();
-    let mut preamble = Preamble::parse(preamble, rev).unwrap();
+#[derive(Debug, Clone)]
+pub struct Frame<'a> {
+    tag: Tag,
+    valid_segments: NonZeroU8,
+    segments: [&'a [u8]; 4],
+}
 
-    Frame::decode(&mut preamble, frame_data).expect("Valid frame should be parseable");
+impl<'a> Frame<'a> {
+    pub fn new(tag: Tag, segments: &[&'a [u8]]) -> Result<Self, String> {
+        if segments.is_empty() || segments.len() > 4 {
+            return Err("Invalid amount of segments".to_string());
+        }
+
+        if segments.last().map(|v| v.is_empty()).unwrap_or(false) {
+            return Err("Last segment in list was empty".to_string());
+        }
+
+        let valid_segments = NonZeroU8::new(segments.len() as _).unwrap();
+
+        let mut segments_out = [EMPTY; 4];
+        segments_out[..segments.len()].copy_from_slice(segments);
+
+        Ok(Self {
+            tag,
+            valid_segments,
+            segments: segments_out,
+        })
+    }
+
+    pub(crate) fn preamble(&self, format: FrameFormat) -> Preamble {
+        let mut segment_details = [SegmentDetail::default(); 4];
+        for (idx, segment) in self.segments().enumerate() {
+            segment_details[idx] = SegmentDetail {
+                length: segment.len() as _,
+                alignment: 1,
+            };
+        }
+
+        Preamble {
+            format,
+            flags: 0,
+            tag: self.tag,
+            segment_count: self.valid_segments,
+            segment_details,
+            _reserved: 0,
+        }
+    }
+
+    pub fn write(&self, format: FrameFormat, output: &mut Vec<u8>) {
+        let preamble = self.preamble(format);
+        preamble.write(output);
+
+        let mut crcs = [0u32; 4];
+
+        for (idx, segment) in self.segments().enumerate() {
+            let crc = CRC.checksum(segment);
+            crcs[idx] = crc;
+            output.extend_from_slice(segment);
+
+            // Apply padding
+            let pad_size = format.segment_pad_size().get();
+            let full_pad_len = segment.len().next_multiple_of(pad_size);
+            let required_padding = full_pad_len - segment.len();
+            output.extend(core::iter::repeat_n(0, required_padding));
+
+            if format == FrameFormat::Rev1Crc && idx == 0 && !segment.is_empty() {
+                output.extend_from_slice(&crc.to_le_bytes());
+            }
+        }
+
+        preamble.write_epilogue(crcs.as_slice(), output);
+    }
+
+    pub(crate) fn decode(preamble: &Preamble, data: &'a [u8]) -> Result<Self, DecodeError> {
+        let mut trailer = data;
+
+        let mut segments = [EMPTY; 4];
+        let mut crc_segment1 = None;
+
+        fn split_segment(buf: &[u8], len: usize) -> Result<(&[u8], &[u8]), DecodeError> {
+            let err = || DecodeError::NotEnoughData {
+                field: Some("segment"),
+                have: buf.len(),
+                need: len,
+            };
+
+            buf.split_at_checked(len).ok_or_else(err)
+        }
+
+        segments[0] = {
+            let segment0 = preamble.segments()[0];
+            let padded_len = segment0
+                .len()
+                .next_multiple_of(preamble.format.segment_pad_size().get());
+
+            let (mut segment_data, left) = split_segment(trailer, padded_len)?;
+            // Shorten to drop potential padding
+            segment_data = &segment_data[..segment0.len()];
+            trailer = left;
+
+            if preamble.format == FrameFormat::Rev1Crc {
+                let err = || DecodeError::NotEnoughData {
+                    field: Some("crc1"),
+                    have: trailer.len(),
+                    need: 4,
+                };
+
+                let (crc, left) = trailer.split_first_chunk::<4>().ok_or_else(err)?;
+
+                crc_segment1 = Some(u32::from_le_bytes(*crc));
+                trailer = left;
+            }
+
+            segment_data
+        };
+
+        for (idx, segment) in preamble.segments().iter().enumerate().skip(1) {
+            let padded_len = segment
+                .len()
+                .next_multiple_of(preamble.format.segment_pad_size().get());
+
+            let (segment_data, left) = split_segment(trailer, padded_len)?;
+
+            // Shorten to drop potential padding
+            segments[idx] = &segment_data[..segment.len()];
+            trailer = left;
+        }
+
+        Self::handle_epilogue(preamble, crc_segment1, &segments, trailer)?;
+
+        Ok(Self {
+            tag: preamble.tag,
+            valid_segments: preamble.segment_count,
+            segments,
+        })
+    }
+
+    fn handle_epilogue(
+        preamble: &Preamble,
+        crc_segment1: Option<u32>,
+        segments: &[&[u8]; 4],
+        trailer: &[u8],
+    ) -> Result<(), DecodeError> {
+        let mut crcs = [0; 4];
+
+        let completed = match preamble.format {
+            FrameFormat::Rev0Crc => {
+                let epilogue = Epilogue::decode(trailer, &mut crcs)?;
+                epilogue.is_completed(preamble.format)
+            }
+            FrameFormat::Rev1Crc => {
+                crcs[0] = crc_segment1.unwrap_or(0xFFFF_FFFF);
+
+                if preamble.need_epilogue_rev2_1() {
+                    let epilogue = Epilogue::decode(trailer, &mut crcs[1..])?;
+                    epilogue.is_completed(preamble.format)
+                } else if !trailer.is_empty() {
+                    return Err(DecodeError::Custom(format!(
+                        "Epilogue should have been empty, but had {} trailing bytes",
+                        trailer.len()
+                    )));
+                } else {
+                    true
+                }
+            }
+            FrameFormat::Rev0Secure => todo!(),
+            FrameFormat::Rev1Secure => {
+                if preamble.need_epilogue_rev2_1() {
+                    if trailer.len() != 16 {
+                        return Err(DecodeError::Custom(format!(
+                            "Expected 16 bytes of epilogue data, got {}",
+                            trailer.len()
+                        )));
+                    }
+
+                    if !trailer[1..].iter().all(|v| *v == 0) {
+                        return Err(DecodeError::Custom(
+                            "Trailing epilogue bytes were not zeroed".to_string(),
+                        ));
+                    }
+
+                    let epilogue = Epilogue::decode(&trailer[..1], &mut [])?;
+                    epilogue.is_completed(preamble.format)
+                } else if !trailer.is_empty() {
+                    return Err(DecodeError::Custom(format!(
+                        "Epilogue should have been empty, but had {} trailing bytes",
+                        trailer.len()
+                    )));
+                } else {
+                    true
+                }
+            }
+        };
+
+        if !completed {
+            return Err(DecodeError::Custom(
+                "Epilogue status did not indicate correct completion".to_string(),
+            ));
+        }
+
+        if preamble.format.has_crc() {
+            for (idx, crc) in crcs.iter().copied().enumerate() {
+                if idx < preamble.segment_count.get() as usize {
+                    let segment = &segments[idx];
+                    let calculated_crc = CRC.checksum(segment);
+                    if crc != calculated_crc {
+                        return Err(DecodeError::Custom(format!(
+                            "Found incorrect CRC 0x{:08X} (expected 0x{:08X}) for segment (#{})",
+                            crc,
+                            calculated_crc,
+                            idx + 1
+                        )));
+                    }
+                } else if crc != 0 {
+                    return Err(DecodeError::Custom(format!(
+                        "Found non-zero CRC (0x{:08X}) for a trailing segment (#{})",
+                        crc,
+                        idx + 1
+                    )));
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub const fn tag(&self) -> Tag {
+        self.tag
+    }
+
+    pub fn segments(&self) -> impl Iterator<Item = &[u8]> {
+        self.segments[..self.valid_segments.get() as usize]
+            .iter()
+            .map(|v| v.as_ref())
+    }
 }
