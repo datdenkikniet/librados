232cf3e1e9f4ed78f940cc5600af818715988fd0
datdenkikniet <jcdra1@gmail.com>
2026-01-19 16:24:15 +0100

    Make decision on not supporting older than NAUTILUS

---
+++ README.md
@@ -0,0 +1,3 @@
+A pure-rust implementation of protocols & messages used for communication in Ceph clusters.
+
+This crate aims to support versions NAUTILUS and up. Support for older versions and protocols (i.e. legacy adresses, msgr1) is not planned.
--- ceph-foundation/src/lib.rs
+++ ceph-foundation/src/lib.rs
@@ -21,3 +21,60 @@ pub struct Timestamp {
 }
 
 write_decode_encode!(Timestamp = tv_sec | tv_nsec);
+
+#[macro_export]
+macro_rules! get_versions_and_data {
+    ($buffer:expr, $version:expr) => {{
+        let [version, compat]: [u8; 2] = $crate::Decode::decode($buffer)?;
+
+        if compat > $version {
+            return Err(DecodeError::Custom(format!(
+                "Incompatible MonInfo struct: we are at {}, received version is {} which claims compatilibity down to {}",
+                $version, version, compat
+            )));
+        }
+
+        let data: &[u8] = $crate::Decode::decode($buffer)?;
+
+        (version, data)
+    }};
+}
+
+struct LenWriter<'a> {
+    buffer: &'a mut Vec<u8>,
+    len_at: usize,
+}
+
+impl Drop for LenWriter<'_> {
+    fn drop(&mut self) {
+        let diff = self.buffer.len().checked_sub(self.len_at).unwrap();
+        let minus_len_bytes = diff.checked_sub(4).unwrap();
+        let as_u32: u32 = minus_len_bytes.try_into().unwrap();
+        self.buffer[self.len_at..self.len_at + 4].copy_from_slice(&as_u32.to_le_bytes());
+    }
+}
+
+impl core::borrow::Borrow<Vec<u8>> for LenWriter<'_> {
+    fn borrow(&self) -> &Vec<u8> {
+        &self.buffer
+    }
+}
+
+impl core::borrow::BorrowMut<Vec<u8>> for LenWriter<'_> {
+    fn borrow_mut(&mut self) -> &mut Vec<u8> {
+        &mut self.buffer
+    }
+}
+
+#[macro_export]
+macro_rules! write_versions_and_data {
+    ($buffer:expr, $version:expr, $compat:expr) => {{
+        $buffer.extend_from_slice(&[$version, $compat]);
+        let len_at = $buffer.len();
+        $buffer.extend_from_slice(&0u32.to_le_bytes());
+        $crate::LenWriter {
+            buffer: $buffer,
+            len_at,
+        }
+    }};
+}
